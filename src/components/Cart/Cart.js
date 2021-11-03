1d7763ba80681da6c932d8439ddca5379d8e8eb8
Kim-Jeongyoung <jeongyoung2@gmail.com>
2021-11-02 21:47:51 -0700

    Added CartItems component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,28 @@
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
-  return;
+  const CartItems = (
+    <ul className={classes['cart-items']}>
+      {[{ id: 'c1', name: 'Green Papaya Sald', amount: 2, price: 22.99 }].map(
+        (item) => (
+          <li>{item.name}</li>
+        )
+      )}
+    </ul>
+  );
+  return (
+    <div>
+      {CartItems}
+      <div className={classes.total}>
+        <span>Total Amount</span>
+        <span>45.98</span>
+      </div>
+      <div>
+        <button className={classes['button--alt']}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
