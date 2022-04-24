dbc8d39172caaca80e8fb8ce9cf1f8db2e7ee339
Jeongyoung Kim <jeongyoung2@gmail.com>
2022-04-24 08:13:18 -0700

    Add comments

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -18,6 +18,7 @@ function App() {
 
   return (
     <CartProvider>
+      {/* to evaluate a dynamic expression */}
       {cartIsShown && <Cart onClose={hideCartHandler} />}
       <Header onShowCart={showCartHandler} />
       <main>
