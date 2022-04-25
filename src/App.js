1bbad7822ff049cd35b9bc567fcde8f001e27b0a
Jeongyoung Kim <jeongyoung2@gmail.com>
2022-04-25 02:50:04 -0700

    143. Using the context

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -18,7 +18,12 @@ function App() {
 
   return (
     <CartProvider>
+      {/* To wrap all components that need access to the cart 
+      => App.js  모든 컴포넌트가 렌더 되는 컴포라서 */}
+      {/* 모달을 눌렀을 때 닫히도록 하는 부분 */}
       {cartIsShown && <Cart onClose={hideCartHandler} />}
+      {/* mamage the visibility of the Cart */}
+      {/* we need to pass a pointer at the function down the props */}
       <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
