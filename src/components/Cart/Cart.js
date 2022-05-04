1fc077bf51a8fde77b7e7994e40e3f9a134c2283
Jeongyoung Kim <jeongyoung2@gmail.com>
2022-05-03 19:10:11 -0700

    223. Adding Better User Feedback

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,6 +26,17 @@ const Cart = (props) => {
     setIsCheckout(true);
   };
 
+  //sending data to the server
+  const submitOrderHandler = (userData) => {
+    fetch("https://food-order-81e9d-default-rtdb.firebaseio.com/orders.json", {
+      method: "POST",
+      body: JSON.stringify({
+        user: userData,
+        orderedItems: cartCtx.items,
+      }),
+    });
+  };
+
   const CartItems = (
     <ul className={classes["cart-items"]}>
       {cartCtx.items.map((item) => (
@@ -62,7 +73,9 @@ const Cart = (props) => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
-      {isCheckout && <Checkout onCancel={props.onClose} />}
+      {isCheckout && (
+        <Checkout onConfirm={submitOrderHandler} onCancel={props.onClose} />
+      )}
       {!isCheckout && modalActions}
     </Modal>
   );
